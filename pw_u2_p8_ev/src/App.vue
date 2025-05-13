11770b666ae920ebfd0df525c1db709e5a3ba34d
Emil2245 <emilmontiel@hotmail.com>
2025-05-12 22:47:09 -0500

    Taller12

---
+++ pw_u2_p8_ev/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <HolaMundo/>
+</template>
+
+<script>
+import HolaMundo from './components/HolaMundo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HolaMundo,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
