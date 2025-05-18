635ec7e147fc72847cdcd839d477bf7cefa083eb
Emil2245 <emilmontiel@hotmail.com>
2025-05-18 00:07:28 -0500

    Deber 4

--- pw_u2_p8_ev/src/App.vue
+++ pw_u2_p8_ev/src/App.vue
@@ -1,34 +1,36 @@
 <template>
   <!-- <HolaMundoView/> -->
   <!-- <ContadorView /> -->
-   <!-- <EstudianteView/> -->
-   <EstudianteTablaView/>
+  <!-- <EstudianteView/> -->
+  <!-- <EstudianteTablaView/> -->
+  <CarroView />
 </template>
 
 <script>
-// import HolaMundoView from '@/views/HolaMundoView.vue'
-// import ContadorView from '@/views/ContadorView.vue'
-import EstudianteTablaView from '@/views/EstudianteTablaView.vue'
+  // import HolaMundoView from '@/views/HolaMundoView.vue'
+  // import ContadorView from '@/views/ContadorView.vue'
+  import EstudianteTablaView from "@/views/EstudianteTablaView.vue";
+  import CarroView from "@/views/CarroView.vue";
 
-export default {
-  name: 'App',
-  components: {
-    // HolaMundoView, 
-    // ContadorView,
-    // EstudianteView,
-    EstudianteTablaView,
-
-  }
-}
+  export default {
+    name: "App",
+    components: {
+      // HolaMundoView,
+      // ContadorView,
+      // EstudianteView,
+      // EstudianteTablaView,
+      CarroView,
+    },
+  };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- pw_u2_p8_ev/src/components/EstudianteTabla.vue
+++ pw_u2_p8_ev/src/components/EstudianteTabla.vue
@@ -1,121 +1,148 @@
 <template>
-    <div class="container">
-        <!-- v-if construye el elemento -->
-        <div v-if="mostrar">
-            <h1>Estudiante Guardado</h1>
-        </div>
-        <!-- v-show lo oculta o lo muestra -->
-        <div v-show="mostrar">
-            <h2>Estudiante Guardado</h2>
-        </div>
-
-        <label for="id_nombre">Nombre:</label>
-        <!-- Directiva v-model: input se vincula a la propiedad reactiva -->
-        <!-- MVC: el modelo es el texto, y el controlador es el JS -->
-        <!-- sale del input (V), (el texto)(M), va al JS y este actualiza el input (C) -->
-        <input v-model="nuevoNombre" id="id_nombre" type="text">
-        <label for="id_apellido">Apellido:</label>
-        <input v-model="nuevoApellido" id="id_apellido" type="text">
-
-        <div class="double_input">
-            <div>
-
-                <label for="id_edad">Edad:</label>
-                <input v-model="nuevaEdad" id="id_edad" type="number">
-            </div>
-            <div>
-
-                <label for="id_genero">Genero:</label>
-                <input v-model="nuevoGenero" id="id_genero" type="text">
-            </div>
-        </div>
-
-        <label for="id_mascota">Mascota:</label>
-        <input v-model="nuevaMascota" id="id_mascota" type="text">
-
-        <button v-on:click="agregarEstudiante()">Agregar Estudiante</button>
-        <p>
-            {{ nuevoNombre }}
-            {{ nuevoApellido }}
-            {{ nuevoGenero }}
-            {{ nuevaEdad }}
-            {{ nuevaMascota }}
-
-        </p>
-        <table>
-            <thead>
-                <tr id="table">
-                    <th> Nombre</th>
-                    <th> Apellido</th>
-                    <th> Genero</th>
-                    <th> Edad</th>
-                    <th> Mascota</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="{ nombre, apellido, genero, edad, mascota } in lista" :key="nombre">
-                    <td>{{ nombre }}</td>
-                    <td>{{ apellido }}</td>
-                    <td>{{ genero }}</td>
-                    <td>{{ edad }}</td>
-                    <td>{{ mascota }}</td>
-                </tr>
-
-            </tbody>
-        </table>
+  <div class="container">
+    <!-- v-if construye el elemento -->
+    <div v-if="mostrar">
+      <h1>Estudiante Guardado</h1>
     </div>
+    <!-- v-show lo oculta o lo muestra -->
+    <div v-show="mostrar">
+      <h2>Estudiante Guardado</h2>
+    </div>
+
+    <label for="id_nombre">Nombre:</label>
+    <!-- Directiva v-model: input se vincula a la propiedad reactiva -->
+    <!-- MVC: el modelo es el texto, y el controlador es el JS -->
+    <!-- sale del input (V), (el texto)(M), va al JS y este actualiza el input (C) -->
+    <input v-model="nuevoNombre" id="id_nombre" type="text" />
+    <label for="id_apellido">Apellido:</label>
+    <input v-model="nuevoApellido" id="id_apellido" type="text" />
+
+    <div class="double_input">
+      <div>
+        <label for="id_edad">Edad:</label>
+        <input v-model="nuevaEdad" id="id_edad" type="number" />
+      </div>
+      <div>
+        <label for="id_genero">Genero:</label>
+        <input v-model="nuevoGenero" id="id_genero" type="text" />
+      </div>
+    </div>
+
+    <label for="id_mascota">Mascota:</label>
+    <input v-model="nuevaMascota" id="id_mascota" type="text" />
+
+    <button v-on:click="agregarEstudiante()">Agregar Estudiante</button>
+    <p>
+      {{ nuevoNombre }}
+      {{ nuevoApellido }}
+      {{ nuevoGenero }}
+      {{ nuevaEdad }}
+      {{ nuevaMascota }}
+    </p>
+    <table>
+      <thead>
+        <tr id="table">
+          <th>Nombre</th>
+          <th>Apellido</th>
+          <th>Genero</th>
+          <th>Edad</th>
+          <th>Mascota</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="{ nombre, apellido, genero, edad, mascota } in lista"
+          :key="nombre"
+        >
+          <td>{{ nombre }}</td>
+          <td>{{ apellido }}</td>
+          <td>{{ genero }}</td>
+          <td>{{ edad }}</td>
+          <td>{{ mascota }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-export default {
+  export default {
     data() {
-        return {
-            nuevoNombre: 'Emil',
-            nuevoApellido: 'Verkade',
-            nuevoGenero: 'Masculino',
-            nuevaEdad: 23,
-            nuevaMascota: 'clotilda la gallina',
-            lista: [
-                { nombre: 'Emil', apellido: 'Verkade', edad: 23, genero: 'Masculino', mascota: 'Cevichocho' },
-                { nombre: 'Mat', apellido: 'Gomez', edad: 23, genero: 'Masculino', mascota: 'Cevichocho' },
-                { nombre: 'Ren', apellido: 'Parra', edad: 23, genero: 'Masculino', mascota: 'Cevichocho' },
-                { nombre: 'Kev', apellido: 'Andrade', edad: 23, genero: 'Masculino', mascota: 'Cevichocho' },
-                { nombre: 'Jos', apellido: 'Teran', edad: 23, genero: 'Masculino', mascota: 'Cevichocho' },
-
-            ],
-            mostrar: false,
-        }
+      return {
+        nuevoNombre: "Emil",
+        nuevoApellido: "Verkade",
+        nuevoGenero: "Masculino",
+        nuevaEdad: 23,
+        nuevaMascota: "clotilda la gallina",
+        lista: [
+          {
+            nombre: "Emil",
+            apellido: "Verkade",
+            edad: 23,
+            genero: "Masculino",
+            mascota: "Cevichocho",
+          },
+          {
+            nombre: "Mat",
+            apellido: "Gomez",
+            edad: 23,
+            genero: "Masculino",
+            mascota: "Cevichocho",
+          },
+          {
+            nombre: "Ren",
+            apellido: "Parra",
+            edad: 23,
+            genero: "Masculino",
+            mascota: "Cevichocho",
+          },
+          {
+            nombre: "Kev",
+            apellido: "Andrade",
+            edad: 23,
+            genero: "Masculino",
+            mascota: "Cevichocho",
+          },
+          {
+            nombre: "Jos",
+            apellido: "Teran",
+            edad: 23,
+            genero: "Masculino",
+            mascota: "Cevichocho",
+          },
+        ],
+        mostrar: false,
+      };
     },
     methods: {
-        agregarEstudiante() {
-            const nuevo = {
-                nombre: this.nuevoNombre,
-                apellido: this.nuevoApellido,
-                edad: this.nuevaEdad,
-                genero: this.nuevoGenero,
-                listaMascotas: this.nuevaListaMascotas,
-            }
-            this.lista.unshift(nuevo);
-            this.mostrar = true;
-            this.limpiar();
-            setTimeout(() => {
-                this.mostrar = false;
-            }, 3000)
-        },
-        limpiar() {
-            this.nuevoNombre = '';
-            this.nuevoApellido = '';
-            this.nuevaEdad = 0;
-            this.nuevoGenero = '';
-            this.nuevaListaMascotas = '';
-        }
-    }
-
-}
+      agregarEstudiante() {
+        const nuevo = {
+          nombre: this.nuevoNombre,
+          apellido: this.nuevoApellido,
+          edad: this.nuevaEdad,
+          genero: this.nuevoGenero,
+          mascota: this.nuevaMascota,
+        };
+        this.lista.unshift(nuevo);
+        this.mostrar = true;
+        // this.limpiar();
+        setTimeout(() => {
+          this.mostrar = false;
+        }, 3000);
+      },
+      limpiar() {
+        this.nuevoNombre = "";
+        this.nuevoApellido = "";
+        this.nuevaEdad = 0;
+        this.nuevoGenero = "";
+        this.nuevaMascota = "";
+      },
+    },
+  };
 </script>
 
 <style scoped>
-.container {
+  .container {
     background: #eee;
     border: 2px solid;
     border-radius: 12px;
@@ -123,19 +150,18 @@ export default {
     margin: 50px auto;
     /* fake center */
     padding: 30px;
+  }
 
-}
-
-label {
+  label {
     display: block;
     color: #333;
     text-align: left;
     padding: 5px;
     font-size: large;
     font-weight: bold;
-}
+  }
 
-input {
+  input {
     background: #4e91f9;
     color: white;
     border: border 1px #ddd;
@@ -144,22 +170,23 @@ input {
     width: auto;
     min-width: 85%;
     margin-bottom: 5px;
-}
+  }
 
-.double_input {
+  .double_input {
     display: grid;
     grid-template-columns: auto auto;
-}
+  }
 
-input:enabled {
+  input:enabled {
     border: none;
-}
+  }
 
-* {
-    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-}
+  * {
+    font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS",
+      sans-serif;
+  }
 
-button {
+  button {
     background: lightseagreen;
     color: white;
     padding: 12px 20px;
@@ -168,19 +195,18 @@ button {
     font-size: 30px;
     cursor: pointer;
     margin: 10px auto;
-}
+  }
 
-table {
+  table {
     /* border-collapse: separate; */
     border-spacing: 0;
     list-style: none;
     margin-top: 30px;
     min-width: 80%;
     margin: auto auto;
+  }
 
-}
-
-thead {
+  thead {
     border: none;
     border-radius: 100px;
     margin-bottom: 5px;
@@ -188,22 +214,20 @@ thead {
     font-size: normal;
     font-weight: bolder;
     border: #333;
+  }
 
-
-}
-
-th {
+  th {
     border-radius: 20px;
     background-color: #ddd;
-}
+  }
 
-td {
+  td {
     background: #f8f9fa;
     border-radius: 5px;
     border: 1px solid lightblue;
-}
+  }
 
-/* table {
+  /* table {
   width: 100%;
   border-collapse: separate;
   border-spacing: 0;
